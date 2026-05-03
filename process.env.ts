4f9d14779810bb921c6dba95d46ad1aaed09e878
robson adonie <robsonadonie47@gmail.com>
2026-05-03 17:19:26 +0300

    s

--- process.env.ts
+++ process.env.ts
@@ -1,2 +1,2 @@
-export const APP_URL = "http://localhost:3100" 
-// export const APP_URL = "https://back-kxkw.onrender.com"  
+// export const APP_URL = "http://localhost:3100" 
+export const APP_URL = "https://autozone-back-1.onrender.com"
